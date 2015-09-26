74e22cc4b9a397333712a32cdbdbd430a5d50528
Shuyang Chen <sc1085@gmail.com>
2015-09-26 15:34:08 -0400

    Got passport to work

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import * as bodyParser from 'body-parser';
 import * as passport from 'passport';
 import {Passport} from 'passport';
 import {Strategy as WindowsLiveStrategy} from 'passport-windowslive';
+import * as session from 'express-session';
 
 export default class Server {
 	
@@ -14,21 +15,30 @@ export default class Server {
 	
 	constructor(public port: number) {
 		this.app = (<any>express).default();
-		
-		this.app.use(express.static(process.cwd() + '/pub'));
-		
 		this.passport = new (<any>passport).Passport();
 		
 		this.passport.use(new WindowsLiveStrategy({
 			clientID: '000000004816E23D',
 			clientSecret: 'sy9Qmud6CIv0sZ1r950C7QaaQPLPsSmY',
-			callbackURL: "https://sanshackgt.azurewebsites.net/auth/windowslive/callback"
+			// callbackURL: "https://sanshackgt.azurewebsites.net/auth/windowslive/callback"
+			callbackURL: "http://lmb1w.canain.com:8080/auth/windowslive/callback"
 		}, this.token.bind(this)));
 		
+		this.passport.serializeUser((user, done) => {
+			done(null, user);
+		});
+		
+		this.passport.deserializeUser((user, done) => {
+			done(null, user);
+		});
+		
+		this.app.use(express.static(process.cwd() + '/pub'));
 		this.app.use(bodyParser.json());
+		this.app.use((<any>session).default({secret: 'sanshackgt'}));
+		this.app.use(this.passport.initialize());
+		this.app.use(this.passport.session());
 		
 		this.app.get('/auth/windowslive', this.passport.authenticate('windowslive', { scope: ['wl.signin'] }));
-		
 		this.app.get('/auth/windowslive/callback', this.passport.authenticate('windowslive'), this.auth.bind(this));
 	}
 	
